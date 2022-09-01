a35880cb9d6baf8ea7e4b87b5a3aad76d14c6c8d
Nigel <nigellu@outlook.com>
2022-09-01 21:14:19 +0800

    React Init

---
+++ src/App.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  console.log(process.env);
+  return (
+    <React.Fragment>
+      <h1>This is my first App</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
